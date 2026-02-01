55fd5793bc5255adbf8404b620d336edd38598db
tm_admin <krestininigor1963@gmail.com>
2026-02-01 17:22:20 +0300

    Fix: clean code and repair Link hydration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,3 @@
-// import React from 'react'
-// import Link from 'next/link'
-// import PerfectBanner from '../components/PerfectBanner'
-// import Newsletter from '../components/Newsletter'
-// import { client } from '../lib/client'
-// import Info from '../components/Info'
-// import Head from 'next/head'
-// import { generateNextSeo } from 'next-seo/pages'
-
-// const Home = () => (
-//   <>
-//     <Head>
-//       {generateNextSeo({
-//         title: 'Macaron Magic',
-//         description: 'Great tasting home-made macarons',
-//       })}
-//     </Head>
-//     <div>
-//       <div className='frontlogo'>
-//         <div className='banner'>
-//           <span className='tagline'>Luxury macarons made by hand</span>
-//           <span>
-//             <Link className='shop-now' href='/shop'>
-//               Shop Now
-//             </Link>
-//           </span>
-//         </div>
-//       </div>
-
-//       <div className='intro'>
-//         <p>
-//           Welcome to Macaron Magic - the home of great-tasting, luxurious
-//           macarons, made by hand here in our workshop in the Peak District.
-//         </p>
-//         <p>
-//           We have carefully chosen a select range of flavors for your delight,
-//           ready for you to enjoy - just imagine...biting into each one, where it
-//           practically melts in your mouth...yum!
-//         </p>
-//         <p>
-//           To start, browse over to our shop where you will see the full range
-//           available - we'll be adding more over time. If you have any questions,
-//           please do let us know - our contact details are at the bottom of this
-//           page.
-//         </p>
-//       </div>
-//       <PerfectBanner />
-//       <Newsletter />
-//     </div>
-//   </>
-// )
-
-// export default Home
-
 import React from 'react'
 import Link from 'next/link'
 import PerfectBanner from '../components/PerfectBanner'
@@ -70,11 +16,9 @@ const Home = ({ products }) => (
       <div className='frontlogo'>
         <div className='banner'>
           <span className='tagline'>Luxury macarons made by hand</span>
-          <span>
-            <Link className='shop-now' href='/shop'>
-              Shop Now
-            </Link>
-          </span>
+          <Link href='/shop' className='shop-now'>
+            Shop Now
+          </Link>
         </div>
       </div>
 
